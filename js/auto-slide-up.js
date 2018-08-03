653b66e29d75029e55ae279113b1dda8e4447a06
enoch <qyqdream@gmail.com>
2018-08-03 09:33:45 +0800

    优化smoothlynavigation / autoslideup

--- js/auto-slide-up.js
+++ js/auto-slide-up.js
@@ -1,31 +1,29 @@
-let targetArea = document.querySelectorAll('[data-x]');
-
-for(let i = 0;i < targetArea.length; i++){
-    targetArea[i].classList.add('offset')
-}
-window.addEventListener('scroll',function(){
-  findMinDistance()
-})
-
-function findMinDistance(){
-  let currentY = window.scrollY + 80;
+!function(){
   let targetArea = document.querySelectorAll('[data-x]');
+  for(let i = 0;i < targetArea.length; i++){
+      targetArea[i].classList.add('offset')
+  }
+  window.addEventListener('scroll',function(){
+    findClosestAndRemoveOffset()
+  })
 
-  let minIndex = 0;
-  for(let i = 1;i < targetArea.length;i++){
-    if(Math.abs(targetArea[i].offsetTop - currentY) < Math.abs(targetArea[minIndex].offsetTop - currentY)){
-      minIndex = i;
+  function findClosestAndRemoveOffset(){
+    let currentY = window.scrollY + 80;
+
+    let minIndex = 0;
+    for(let i = 1;i < targetArea.length;i++){
+      if(Math.abs(targetArea[i].offsetTop - currentY) < Math.abs(targetArea[minIndex].offsetTop - currentY)){
+        minIndex = i;
+      }
     }
+    let navA = document.querySelector('a[href="#'+ targetArea[minIndex].id +'"]');
+    let navLi = navA.parentNode.parentNode.children;
+    for(let i = 0; i < navLi.length; i++){
+      navLi[i].classList.remove('highlight')
+    }
+    navA.parentNode.classList.add('highlight');
+
+    let currentArea = targetArea[minIndex];
+    currentArea.classList.remove('offset');
   }
-  let navA = document.querySelector('a[href="#'+ targetArea[minIndex].id +'"]');
-  let navLi = navA.parentNode.parentNode.children;
-  for(let i = 0; i < navLi.length; i++){
-    navLi[i].classList.remove('highlight')
-  }
-  navA.parentNode.classList.add('highlight');
-
-  let currentArea = targetArea[minIndex];
-  currentArea.classList.remove('offset');
-
-}
-
+}.call()
--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -1,29 +1,48 @@
-let aTags = document.querySelectorAll('.navBar nav > ul > li > a');
-for(let i = 0 ; i < aTags.length ; i++){
-  aTags[i].onclick = function(ele){
-    ele.preventDefault();
-    let targetY = document.querySelector(ele.target.getAttribute('href')).offsetTop - 80;
-    let currentY = window.scrollY;
-    let distance = currentY - targetY;
-    let time = Math.abs((distance/100) * 300);
-    if(time > 500){ time = 500 ;}
-
-    // Setup the animation loop.
-    function animate(time) {
-        requestAnimationFrame(animate);
-        TWEEN.update(time);
+!function(){
+  let view = document.querySelectorAll('.navBar nav > ul > li > a')
+  let controller = {
+    view : null,
+    currentY : null,
+    targetY : null,
+    distance : null,
+    time : null,
+    init : function(view){
+      this.view = view
+      this.bindEvents()
+    },
+    bindEvents : function(){
+      let view = this.view
+      for(let i = 0 ; i < view.length ; i++){
+        view[i].onclick = function(ele){
+          ele.preventDefault();
+          this.targetY = document.querySelector(ele.target.getAttribute('href')).offsetTop - 80;
+          this.currentY = window.scrollY;
+          this.distance = this.currentY - this.targetY;
+          this.time = Math.abs((this.distance/100) * 300);
+          if(this.time > 500){ this.time = 500 ;}
+          this.initAnimation()
+          this.smoothAnimation()
+        }.bind(controller)
+      }
+    },
+    initAnimation : function(){
+      // Setup the animation loop.
+      function animate(time) {
+          requestAnimationFrame(animate);
+          TWEEN.update(time);
+      }
+      requestAnimationFrame(animate);
+    },
+    smoothAnimation : function(){
+      var coords = {  y: this.currentY }; 
+      var tween = new TWEEN.Tween(coords) 
+              .to({  y: this.targetY }, this.time) 
+              .easing(TWEEN.Easing.Quadratic.InOut) 
+              .onUpdate(function() { 
+                  window.scrollTo(0,coords.y)
+              })
+              .start(); 
     }
-    requestAnimationFrame(animate);
-
-    var coords = {  y: currentY }; 
-    var tween = new TWEEN.Tween(coords) 
-            .to({  y: targetY }, time) 
-            .easing(TWEEN.Easing.Quadratic.InOut) 
-            .onUpdate(function() { 
-                window.scrollTo(0,coords.y)
-            })
-            .start(); 
   }
-}
-
-
+  controller.init(view)
+}.call()
